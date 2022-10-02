2a11c734b4d80fa27305d8dd3c59388d33ab55d5
BoD <BoD@JRAF.org>
2022-10-02 15:18:42 +0200

    Dockerization

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 A bridge to transform a Twitter list into an RSS feed.
+
+## Docker instructions
+
+### Building and pushing the image to Docker Hub
+
+```
+docker image rm bodlulu/twitter-to-rss:latest
+DOCKER_USERNAME=<your docker hub login> DOCKER_PASSWORD=<your docker hub password> ./gradlew dockerPushImage
+```
+
+### Running the image
+
+```
+docker pull bodlulu/twitter-to-rss
+docker run -p <PORT TO LISTEN TO>:8080 bodlulu/twitter-to-rss
+```
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,9 @@
+import com.bmuschko.gradle.docker.tasks.image.DockerBuildImage
+
 plugins {
     kotlin("jvm")
     application
+    id("com.bmuschko.docker-java-application")
 }
 
 group = "org.jraf"
@@ -24,5 +27,22 @@ application {
     mainClass.set("org.jraf.twittertorss.MainKt")
 }
 
+docker {
+    javaApplication {
+        maintainer.set("BoD <BoD@JRAF.org>")
+        ports.set(listOf(8080))
+        images.add("bodlulu/${rootProject.name}:latest")
+    }
+    registryCredentials {
+        username.set(System.getenv("DOCKER_USERNAME"))
+        password.set(System.getenv("DOCKER_PASSWORD"))
+    }
+}
+
+tasks.withType<DockerBuildImage> {
+    platform.set("linux/amd64")
+}
+
 // `./gradlew refreshVersions` to update dependencies
 // `./gradlew distZip` to create a zip distribution
+// `DOCKER_USERNAME=<your docker hub login> DOCKER_PASSWORD=<your docker hub password> ./gradlew dockerPushImage` to build and push the image
